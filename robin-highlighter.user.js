66459b1d66d18a3d755ce57b33dc6d460171cd64
Chris Stevens <chris27stevens@gmail.com>
2016-04-01 19:11:47 +0100

    Get it to work, who cares about commit messages...

--- robin-highlighter.user.js
+++ robin-highlighter.user.js
@@ -1,23 +1,20 @@
 // ==UserScript==
 // @name         Robin Highlighter
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Highlights your username in Robin chats!
 // @author       Chr12t0pher
 // @include      https://www.reddit.com/robin*
 // @grant        none
 // ==/UserScript==
 
-(function () {
-    'use strict';
 
-    var username = $("span.user").find("a").first().text().toLowerCase();
-
-    $("#robinChatMessageList").bind("DOMNodeInserted", function() {
-        var x = $("#robinChatMessageList .robin-message").last();
-        var message = x.find(".robin-message--message");
-        if (message.text().toLowerCase() == username) {
-            message.css("background-color", "yellow");
-        }
-    });
+'use strict';
+var username = $("span.user").find("a").first().text().toLowerCase();
+$("#robinChatMessageList").bind("DOMNodeInserted", function() {
+    var x = $("#robinChatMessageList .robin-message").last();
+    var message = x.find(".robin-message--message");
+    if (message.text().toLowerCase() == username) {
+        message.css("background-color", "yellow");
+    }
 });
