960368cd2ea5b2500d92b91ff97e20da9830e898
Corey Taylor <taylorcoreyd@icloud.com>
2016-04-03 01:20:35 -0500

    VIP feature created
    VIP feature allows you to create a list of VIPs, which highlights text
    by users on your VIP list.

--- robin-highlighter.user.js
+++ robin-highlighter.user.js
@@ -10,11 +10,51 @@
 
 
 'use strict';
+
+var vips = []
+
 var username = $("span.user").find("a").first().text().toLowerCase();
 $("#robinChatMessageList").bind("DOMNodeInserted", function() {
+
     var x = $("#robinChatMessageList .robin-message").last();
     var message = x.find(".robin-message--message");
+<<<<<<< Updated upstream
     if (message.text().toLowerCase().search(username) != -1) {
+=======
+    var lastUser = x.find(".robin-message--from.robin--username");
+
+    // highlight our username
+    if (message.text().toLowerCase() == username) {
+>>>>>>> Stashed changes
         message.css("background-color", "yellow");
+    }
+
+    // Highlight saved users.
+    for(var i in vips) {
+      if (lastUser.text().toLowerCase() == vips[i]) {
+        message.css("background-color", "#b3ffb3")
+      }
+    }
+});
+
+var inputBox = $(document).find(".c-form-control.text-counter-input");
+// on input = whenever user types in or makes some change.
+inputBox.on("input", function() {
+    // Looking for command
+    var command = ".vip ";
+    var cmdExec = "!";
+
+    // Get the value from the inputBox
+    var inVal = $( this ).val();
+
+    // If command is found
+    if (inVal.substring(0, 5) == command && inVal.slice(-1) == cmdExec) {
+        // get username
+        var vip = inVal.substring(5, inVal.length - 1);
+        vips.push(vip.toLowerCase());
+        console.log("added to vips: " + vip);
+
+        // clear the command
+        $( this ).val("");
     }
 });
